2cab0a6938c3c970c62da62cce81a0a67820a074
Crackvignoule <kiki.kalagan@gmail.com>
2024-10-14 15:59:45 +0200

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sounddevice as sd
 def create_sine_wave(frequency, duration, sample_rate):
     t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
     signal = np.sin(2 * np.pi * frequency * t)
-    return signal
+    return t, signal
 
 # 1.c. Quantifier ce signal en utilisant différentes résolutions
 def quantize_signal(signal, bits):
@@ -40,7 +40,7 @@ def main():
     frequency = 2000  # 2kHz
     duration = 3  # 3 seconds
     sample_rate = frequency * 10  # 10 samples per period
-    signal = create_sine_wave(frequency, duration, sample_rate)
+    t, signal = create_sine_wave(frequency, duration, sample_rate)
 
     # Reproduire cette tonalité sur les haut-parleurs de votre ordinateur
     sd.play(signal, sample_rate)
@@ -67,44 +67,63 @@ def main():
     lost_signal_1 = simulate_packet_loss(quantized_signal_8bit, sample_rate, packet_size, loss_probability_1)
     lost_signal_2 = simulate_packet_loss(quantized_signal_8bit, sample_rate, packet_size, loss_probability_2)
 
-    slice_length = 200
+    slice_length = 100
+    t_slice = t[:slice_length]
 
     # Plotting the signals for visualization
     plt.figure(figsize=(15, 18))
 
-    plt.subplot(8, 1, 1)
-    plt.plot(signal[:slice_length], label='Original Signal')
+    plt.subplot(4, 2, 1)
+    plt.plot(t_slice, signal[:slice_length], label='Original Signal')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
     plt.legend()
 
-    plt.subplot(8, 1, 2)
-    plt.plot(quantized_signal_6bit[:slice_length], label='Quantized Signal (6-bit)')
+    plt.subplot(4, 2, 3)
+    plt.plot(t_slice, quantized_signal_6bit[:slice_length], label='Quantized Signal (6-bit)')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
     plt.legend()
 
-    plt.subplot(8, 1, 3)
-    plt.plot(quantized_signal_4bit[:slice_length], label='Quantized Signal (4-bit)')
+    plt.subplot(4, 2, 5)
+    plt.plot(t_slice, quantized_signal_4bit[:slice_length], label='Quantized Signal (4-bit)')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
     plt.legend()
 
-    plt.subplot(8, 1, 4)
-    plt.plot(quantized_signal_3bit[:slice_length], label='Quantized Signal (3-bit)')
+    plt.subplot(4, 2, 7)
+    plt.plot(t_slice, quantized_signal_3bit[:slice_length], label='Quantized Signal (3-bit)')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
     plt.legend()
 
-    plt.subplot(8, 1, 5)
-    plt.plot(quantized_signal_2bit[:slice_length], label='Quantized Signal (2-bit)')
+    plt.subplot(4, 2, 2)
+    plt.plot(t_slice, quantized_signal_2bit[:slice_length], label='Quantized Signal (2-bit)')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
     plt.legend()
 
-    plt.subplot(8, 1, 6)
-    plt.plot(quantized_signal_1bit[:slice_length], label='Quantized Signal (1-bit)')
+    plt.subplot(4, 2, 4)
+    plt.plot(t_slice, quantized_signal_1bit[:slice_length], label='Quantized Signal (1-bit)')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
     plt.legend()
 
-    plt.subplot(8, 1, 7)
-    plt.plot(delayed_signal[:slice_length], label='Delayed Signal')
+    plt.subplot(4, 2, 6)
+    plt.plot(t_slice, delayed_signal[:slice_length], label='Delayed Signal')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
     plt.legend()
 
-    plt.subplot(8, 1, 8)
-    plt.plot(lost_signal_1[:slice_length], label='Lost Signal (p=10^-3)')
-    plt.plot(lost_signal_2[:slice_length], label='Lost Signal (p=10^-2)')
+    plt.subplot(4, 2, 8)
+    plt.plot(t_slice, lost_signal_1[:slice_length], label='Lost Signal (p=10^-3)')
+    plt.plot(t_slice, lost_signal_2[:slice_length], label='Lost Signal (p=10^-2)')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
     plt.legend()
 
+    plt.tight_layout(h_pad=4)
+    plt.subplots_adjust(bottom=0.06)
     plt.show()
 
 if __name__ == "__main__":
